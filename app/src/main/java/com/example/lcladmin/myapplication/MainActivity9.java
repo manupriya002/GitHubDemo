dd95c07e588fefa480be1445d526aab1de328cf8
priya2 <priya.kri.sah@gmail.com>
2017-07-05 22:50:23 +0530

    added activity

---
+++ app/src/main/java/com/example/lcladmin/myapplication/MainActivity9.java
@@ -0,0 +1,16 @@
+package com.example.lcladmin.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+public class MainActivity9 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //hkjkjkj
+        Log.i("hjsdhf", "hfhahdfdajf");
+    }
+}
